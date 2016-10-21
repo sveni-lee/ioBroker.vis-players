4c088dbd6d4b86774682a64783fc4dfd3d09418b
instalator <vvvalt@mail.ru>
2016-10-21 13:19:02 +0700

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+![Logo](admin/preview.png)
+# ioBroker.vis-players-widgets
+[![NPM version](https://img.shields.io/npm/v/ioBroker.vis-players-widgets.svg)](https://www.npmjs.com/package/ioBroker.vis-players-widgets)
+[![Downloads](https://img.shields.io/npm/dm/ioBroker.vis-players-widgets.svg)](https://www.npmjs.com/package/ioBroker.vis-players-widgets)
+
+[![NPM](https://nodei.co/npm/ioBroker.vis-players-widgets.png?downloads=true)](https://nodei.co/npm/ioBroker.vis-players-widgets/)
+
+## Changelog
+
+#### 0.0.1
+* (instalator) initial (21.10.2016)
---
+++ widgets/players-widgets/js/players-widgets.js
@@ -0,0 +1,68 @@
+/*
+    ioBroker.vis players-widgets Widget-Set
+
+    version: "0.0.1"
+
+    Copyright 10.2015-2016 instalator<vvvalt@mail.ru>
+
+*/
+"use strict";
+
+// add translations for edit mode
+if (vis.editMode) {
+    $.extend(true, systemDictionary, {
+        "myColor":          {"en": "myColor",       "de": "mainColor",  "ru": "Мой цвет"},
+        "myColor_tooltip":  {
+            "en": "Description of\x0AmyColor",
+            "de": "Beschreibung von\x0AmyColor",
+            "ru": "Описание\x0AmyColor"
+        },
+        "htmlText":         {"en": "htmlText",      "de": "htmlText",   "ru": "htmlText"},
+        "group_extraMyset": {"en": "extraMyset",    "de": "extraMyset", "ru": "extraMyset"},
+        "extraAttr":        {"en": "extraAttr",     "de": "extraAttr",  "ru": "extraAttr"}
+    });
+}
+
+// add translations for non-edit mode
+$.extend(true, systemDictionary, {
+    "Instance":  {"en": "Instance", "de": "Instanz", "ru": "Инстанция"}
+});
+
+// this code can be placed directly in players-widgets.html
+vis.binds.players-widgets = {
+    version: "0.0.1",
+    showVersion: function () {
+        if (vis.binds.players-widgets.version) {
+            console.log('Version players-widgets: ' + vis.binds.players-widgets.version);
+            vis.binds.players-widgets.version = null;
+        }
+    },
+	createWidget: function (widgetID, view, data, style) {
+        var $div = $('#' + widgetID);
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function () {
+                vis.binds.players-widgets.createWidget(widgetID, view, data, style);
+            }, 100);
+        }
+
+        var text = '';
+        text += 'OID: ' + data.oid + '</div><br>';
+        text += 'OID value: <span class="myset-value">' + vis.states[data.oid + '.val'] + '</span><br>';
+        text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + '</span><br>';
+        text += 'extraAttr: ' + data.extraAttr + '<br>';
+        text += 'Browser instance: ' + vis.instance + '<br>';
+        text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || '') + '</textarea><br>';
+
+        $('#' + widgetID).html(text);
+
+        // subscribe on updates of value
+        if (data.oid) {
+            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                $div.find('.players-widgets-value').html(newVal);
+            });
+        }
+    }
+};
+	
+vis.binds.players-widgets.showVersion();
