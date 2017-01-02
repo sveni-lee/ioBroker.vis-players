deb9de28f7d6b5f551cec29e3ee2a33f4c20bd68
instalator <vvvalt@mail.ru>
2017-01-02 19:18:03 +0700

    add pre playlist

--- widgets/players/js/players.js
+++ widgets/players/js/players.js
@@ -38,20 +38,20 @@ vis.binds.players = {
         }
     },
     states: {
-        oid_play:    {val: undefined, selector: '', objName: 'play'},
-        oid_next:    {val: undefined, selector: '', objName: 'next'},
-        oid_prev:    {val: undefined, selector: '', objName: ''},
-        oid_stop:    {val: undefined, selector: '', objName: ''},
-        oid_pause:   {val: undefined, selector: '', objName: ''},
-        oid_seek:    {val: 0,         selector: '', objName: ''},
-        oid_vol:     {val: 0,         selector: '', objName: ''},
-        oid_mute:    {val: undefined, selector: '', objName: ''},
-        oid_shuffle: {val: undefined, selector: '', objName: ''},
-        oid_repeat:  {val: undefined, selector: '', objName: ''},
-        oid_artist:  {val: '',        selector: '', objName: ''},
-        oid_title:   {val: '',        selector: '', objName: ''},
-        oid_album:   {val: '',        selector: '', objName: ''},
-        oid_bitrate: {val: '',        selector: '', objName: ''}
+        oid_play:    {val: undefined, role: 'button.play', selector: '', objName: 'play'},
+        oid_next:    {val: undefined, role: 'button.next', selector: '', objName: 'next'},
+        oid_prev:    {val: undefined, role: 'button.prev', selector: '', objName: ''},
+        oid_stop:    {val: undefined, role: 'button.stop', selector: '', objName: ''},
+        oid_pause:   {val: undefined, role: 'button.pause', selector: '', objName: ''},
+        oid_seek:    {val: 0,         role: 'media.seek', selector: '', objName: ''},
+        oid_vol:     {val: 0,         role: 'level.volume', selector: '', objName: ''},
+        oid_mute:    {val: undefined, role: 'media.mute', selector: '', objName: ''},
+        oid_shuffle: {val: undefined, role: 'media.mode.shuffle', selector: '', objName: ''},
+        oid_repeat:  {val: undefined, role: 'media.mode.repeat', selector: '', objName: ''},
+        oid_artist:  {val: '',        role: 'media.artist', selector: '', objName: ''},
+        oid_title:   {val: '',        role: 'media.title', selector: '', objName: ''},
+        oid_album:   {val: '',        role: 'media.album', selector: '', objName: ''},
+        oid_bitrate: {val: '',        role: 'media.bitrate', selector: '', objName: ''}
     },
     
     createWidgetWinampPlayer: function (widgetID, view, data, style) { //tplWinampPlayer
@@ -66,40 +66,26 @@ vis.binds.players = {
             }, 100);
         }
         $(function() {
-            //var vol_slider = $('#winamp-vol_slider');
             vol_slider.slider({
                 range: "min",
                 min: 0,
                 value: vis.states['data.oid_vol.val'],//states.oid_vol.val,
-                start: function(event,ui) {
-                },
+                start: function(event,ui) {},
                 slide: function(event, ui) {
-                    //console.log(ui);
-                    //var value = vol_slider.slider('value');
                     vis.setValue(data.oid_vol, ui.value);
                 },
-                stop: function(event,ui) {
-
-                }
+                stop: function(event,ui) {}
             });
-
-        /*});
-        $(function() {*/
-           // var seek_slider = $('#winamp-seek_slider');
             seek_slider.slider({
                 range: "min",
                 min: 0,
                 max: 100,
                 value: 0,//states.oid_seek.val,
-                start: function(event,ui) {
-
-                },
+                start: function(event,ui) {},
                 slide: function(event, ui) {
-                    //console.log(ui.value);
                     vis.setValue(data.oid_seek, ui.value);
                 },
-                stop: function(event,ui) {
-                }
+                stop: function(event,ui) {}
             });
         });
         function updateStates() {
@@ -124,11 +110,10 @@ vis.binds.players = {
             } else {
                 $('.winamp-shuffle').css("display","none");
             }
-
-            vol_slider.slider( "value", states.oid_vol.val );
-            seek_slider.slider( "value", parseInt(states.oid_seek.val) );
-
+            vol_slider.slider("value", states.oid_vol.val);
+            seek_slider.slider("value", parseInt(states.oid_seek.val));
         }
+
         $(".winamp-btn").on("click", function(e){
             switch (e.target.id) {
                 case 'mute':
@@ -185,7 +170,108 @@ vis.binds.players = {
         }
         // initial update
         updateStates();
+    },
+    createWidgetWinampPlaylist: function (widgetID, view, data, style) { //tplWinampPlayer
+        var $div = $('#' + widgetID);
+        //var states = {};
+        var playlist;
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function () {
+                vis.binds.players.createWidgetWinampPlaylist(widgetID, view, data, style);
+            }, 100);
+        }
+
+        function updateStates(pl) {
+           /* states = JSON.parse(JSON.stringify(vis.binds.players.states));
+            for (var s in states) {
+                if( states.hasOwnProperty(s)){
+                    if (data[s] && data[s] !== 'nothing_selected') { states[s].val = vis.states[data[s] + '.val']; }
+                }
+            }*/
+            playlist = JSON.parse(pl);
+
+                /*$div.find("#playListContainer").empty();
+                _playlist.forEach(function(item, i, arr) {
+                    $div.find("#playListContainer").append("<li class='item"+(i+1)+"'>"+(i+1)+' - '+_playlist[i].label+"</li>");
+                });
+                $div.find("#playListContainer .item"+(parseInt(vis.states[data.oid_position + '.val'])+1)).addClass("active");
+                $div.find('#playListContainer').on('click', "li", function(){
+                    var n=$(this).index();
+                    vis.setValue(data.oid_position, n);
+                });*/
+            console.log(playlist);
+             $div.find("#winamp-playlist-container").empty();
+             playlist.forEach(function(item, i, plst) {
+                 var obj = playlist[i];
+                 console.log(obj);
+                 $div.find("#winamp-playlist-container").append("<li class='item"+(i+1)+"'>"+(i+1)+' - '+ obj.file+"</li>");
+             });
+        }
+        
+        $(".winamp-btn").on("click", function(e){
+
+        });
+
+        //debugger;
+        // subscribe on updates of values
+        if (data.oid_playlist) {
+            vis.states.bind(data.oid_playlist + '.val', function (e, newVal, oldVal) {
+                updateStates(newVal);
+            });
+        }
+        // initial update
+        //updateStates();
     }
+    
+    /*
+     {
+     "items": [{
+     "album": "",
+     "artist": [],
+     "episode": -1,
+     "fanart": "",
+     "file": "smb://192.168.1.10/F/CAFE SOCIETY/BDMV/index.bdmv",
+     "genre": [],
+     "label": "index.bdmv",
+     "playcount": 1,
+     "rating": 0,
+     "season": -1,
+     "showtitle": "",
+     "thumbnail": "",
+     "title": "",
+     "track": -1,
+     "type": "unknown",
+     "year": 1969
+     }],
+     "limits": {
+     "end": 1,
+     "start": 0,
+     "total": 1
+     }
+     }
+     */
+    
+    
+    
+    
+    
 };
+if (vis.editMode) {
+    /*vis.binds.players = function (widgetID, view, newId, attr, isCss) {
+        console.log('---------: ' + widgetID +' - '+view+' - '+newId+' - '+attr+' - '+isCss);
+        newId = newId ? newId.substring(0, newId.length - attr.length + 'oid_'.length) : '';
+        var changed = [];
+        for (var s in vis.binds.players.states) {
+            if (s === 'oid_alias' || !vis.binds.players.states[s].objName) continue;
+            if (vis.objects[newId + vis.binds.players.states[s].objName]) {
+                changed.push(s);
+                vis.views[view].widgets[widgetID].data[s] 	= newId + vis.binds.players.states[s].objName;
+                vis.widgets[widgetID].data[s] 				= newId + vis.binds.players.states[s].objName;
+            }
+        }
+        return changed;
+    };*/
+}
 	
 vis.binds.players.showVersion();
